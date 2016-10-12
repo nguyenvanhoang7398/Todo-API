e7a45e54f0b2cf4704d76a21b823b8af5937f755
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2016-10-12 18:23:32 +0800

    Add POST for TODOS

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
+/*var todos = [{
     id: 1,
     description: 'Complete CS1010 lab assignment',
     completed: false
@@ -13,12 +20,14 @@ var todos = [{
     id: 3,
     description: 'Jogging at NUS field',
     completed: true
-}];
+}];*/
 
+// Get /todos
 app.get('/todos', function (req, res) {
     res.json(todos);
 });
 
+// Get /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoId = req.params.id;
     var matchedTodo;
@@ -37,10 +46,20 @@ app.get('/todos/:id', function (req, res) {
     }
 })
 
+// Get /
 app.get('/', function (req, res) {
     res.send('Todo API Root');
 });
 
+// POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    
+    body.id = todoNextId++;
+    todos.push(body);
+    
+    res.json(body);
+});
 app.listen(PORT, function() {
     console.log('Express listening on port ' + PORT + '!');
 });
