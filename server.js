1a5f36169f628f5cca6a5f54e09ad96f6786ecd0
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2016-10-12 21:13:46 +0800

    Add DELETE for TODOS

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/todos/:id', function (req, res) {
     if (matchedTodo) {
         res.send(matchedTodo);
     } else {
-        res.status(404).send('Todo not found!');
+        res.status(404).json({"error": "todo not found"});
     }
 })
 
@@ -50,7 +50,7 @@ app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed');
     
     if (!_.isBoolean(body.completed) || !_.isString(body.description) || !body.description.trim().length) {
-        return res.status(400).send('Bad data provided');
+        return res.status(400).json({"error": "Bad data provided"});
     }
     
     body.description = body.description.trim();
@@ -59,6 +59,20 @@ app.post('/todos', function (req, res) {
     
     res.json(body);
 });
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    
+    if (matchedTodo) {
+        todos = _.without(todos, matchedTodo);
+        
+        res.json(matchedTodo);
+    } else {
+        res.status(404).json({"error": "No todo found with that id"});
+    }
+})
 app.listen(PORT, function() {
     console.log('Express listening on port ' + PORT + '!');
 });
