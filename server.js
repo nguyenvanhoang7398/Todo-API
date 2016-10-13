4d43eec4483e6dd86312f9e0484f9f0ec6d23597
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2016-10-14 00:08:57 +0800

    Update SQL database by PUT /TODO/:id

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ app.get('/todos', function(req, res) {
 
     db.todo.findAll({
         where: where
-    }).then(function (filteredTodos) {
+    }).then(function(filteredTodos) {
         res.status(200).json(filteredTodos);
-    }, function (e) {
+    }, function(e) {
         res.status(500).json(e);
     });
 });
@@ -97,12 +97,12 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    
+
     db.todo.destroy({
         where: {
             id: todoId
         }
-    }).then(function (rowDeleted) {
+    }).then(function(rowDeleted) {
         if (rowDeleted === 0) {
             res.status(404).json({
                 "error": "Todo not found"
@@ -110,7 +110,7 @@ app.delete('/todos/:id', function(req, res) {
         } else {
             res.status(204).send();
         }
-    }).then(function () {
+    }).then(function() {
         res.status(500);
     });
 });
@@ -118,36 +118,31 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {
-        id: todoId
-    });
-    var body = _.pick(req.body, 'description', 'completed');
-    var validAttributes = {};
+    var body = _.pick(req.body, 'completed', 'description');
+    var attributes = {};
 
-    if (!matchedTodo) {
-        return res.status(404).json({
-            "error": "No todo found with that id"
-        });
+    if (body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
+    }
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
     }
 
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validAttributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')) {
-        return res.status(400).json({
-            "error": "bad data provided"
-        });
-    }
-
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-        validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        return res.status(400).json({
-            "error": "bad data provided"
-        });
-    }
-
-    _.extend(matchedTodo, validAttributes);
-    res.json(matchedTodo);
+    db.todo.findById(todoId).then(function(todo) {
+        if (!!todo) {
+            todo.update(attributes).then(function(todo) {
+                res.json(todo.toJSON());
+            }, function(e) {
+                res.status(400).json(e);
+            });
+        } else {
+            res.status(404).json({
+                "error": "Todo not found"
+            });
+        }
+    }, function() {
+        res.status(500).send();
+    })
 });
 
 db.sequelize.sync().then(function() {
