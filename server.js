80358ae64cf003c53f3ff36729540205f83dbe07
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2016-10-15 19:07:38 +0800

    Hash and salt password

--- server.js
+++ server.js
@@ -121,14 +121,14 @@ app.put('/todos/:id', function(req, res) {
 app.post('/users', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
 
-    db.user.create(body).then(function (body) {
-        res.status(200).json(body.toJSON());
+    db.user.create(body).then(function (user) {
+        res.status(200).json(user.toPublicJSON());
     }, function (e) {
         res.status(400).json(e);
-    })
-})
+    });
+});
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force: true}).then(function() {
     app.listen(PORT, function() {
         console.log('Express listening on port ' + PORT + '!');
     });
