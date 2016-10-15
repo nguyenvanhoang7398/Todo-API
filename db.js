5eb18f06dcbbf6550f64115cb5b0b1efc2a77ccf
NguyenVanHoang <nguyenvanhoang7398@gmail.com>
2016-10-16 01:26:45 +0800

    Add DELETE /users/login

--- db.js
+++ db.js
@@ -18,9 +18,14 @@ var db = {};
 db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
 db.user = sequelize.import(__dirname + '/models/user.js');
+
+db.token = sequelize.import(__dirname + '/models/token.js');
 
 db.todo.belongsTo(db.user);
 db.user.hasMany(db.todo);
+
+
 
 module.exports = db;
--- server.js
+++ server.js
@@ -151,19 +151,32 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
     var body = _.pick(req.body, 'email', 'password');
+    var userInstance;
 
     db.user.authenticate(body).then(function(user) {
         var token = user.generateToken('authentication');
+        userInstance = user;
 
-        if (token) {
-            res.header('Auth', user.generateToken('authentication')).json(user.toPublicJSON());
-        } else {
-            res.status(401).send();
-        }
-    }, function() {
+        return db.token.create({
+            token: token
+        });
+    }).then(function(tokenInstance) {
+        res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+    }).catch (function(e) {
+        console.log(e);
         res.status(401).send();
     });
 })
+
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function(req, res) {
+    req.token.destroy().then(function () {
+        res.status(204).send();
+    }).catch(function (e) {
+        console.log(e);
+        res.status(500).send();
+    })
+});
 
 db.sequelize.sync({
     force: true
